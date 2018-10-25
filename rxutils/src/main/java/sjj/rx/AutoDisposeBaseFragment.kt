8096c172e481c446d4b0cbf35269ebad0e4536d2
shenjianjun <R5LCeHRvfsmTqPOl>
2018-10-25 15:05:09 +0800

    rxutils==》fragment activity 扩展

---
+++ rxutils/src/main/java/sjj/rx/AutoDisposeBaseFragment.kt
@@ -0,0 +1,18 @@
+package sjj.rx
+
+import android.support.v4.app.Fragment
+import io.reactivex.disposables.Disposable
+
+open class AutoDisposeBaseFragment : Fragment() {
+    fun Disposable.destroy(onceKey: String? = null) {
+        destroy(onceKey, lifecycle)
+    }
+
+    fun Disposable.stop(onceKey: String? = null) {
+        stop(onceKey, lifecycle)
+    }
+
+    fun Disposable.pause(onceKey: String? = null) {
+        pause(onceKey, lifecycle)
+    }
+}
