1396ebe4a27b0d3537b808173a02879f88b3ba3f
shenjianjun <R5LCeHRvfsmTqPOl>
2018-10-25 14:57:52 +0800

    rxutils

--- app/src/main/java/com/example/sjj/android_libs/MainActivity.kt
+++ app/src/main/java/com/example/sjj/android_libs/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.sjj.android_libs
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
 }
